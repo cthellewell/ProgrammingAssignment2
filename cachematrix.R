026c7c165fb621da238ce082ee95e8d9a7f30317
Chris Hellewell <cthellewell@gmail.com>
2014-12-18 11:23:30 -0700

    completed assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object
+## that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    ## initialize the inverse
+    i <- NULL
+    
+    ## define a function to set the value of the "matrix"
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    
+    ## define a function to get the matrix
+    get <- function() x
+    
+    ## define a function to store the inverse matrix in the cache
+    setinverse <- function(inv) i <<- inv
+    
+    ## define a function to get the value of the inverse stored
+    ## in the cache
+    getinverse <- function() i
+    
+    ## return the list of functions defined above
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+    
 }
 
 
-## Write a short comment describing this function
-
+## This function computes the inverse of the special
+## "matrix" returned by `makeCacheMatrix` above. If the inverse has
+## already been calculated (and the matrix has not changed), then
+## `cacheSolve` retrieves the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+    ## get the inverse of our "special" matrix
+    i <- x$getinverse()
+    
+    ## if the inverse has already been caculated and
+    ## is in the cache, return the cached value
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    
+    ## the inverse hasn't been calculated yet
+    ## grab matrix from our special "matrix"
+    data <- x$get()
+
+    ## calculate the inverse of the matrix
+    ## Assume that the matrix supplied is always invertible.
+    i <- solve(data, ...)
+    
+    ## save the inverse in the cache
+    x$setinverse(i)
+    
+    ## return the inverse
+    i
+    
 }
